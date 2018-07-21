a456c3a517a8fcc964055927e2b07e2e69624fdb
ksunandr <ksun.andr@gmail.com>
2018-07-21 12:49:43 +0300

    corrections new empty the first test

--- src/test/java/tools/CulcTest.java
+++ src/test/java/tools/CulcTest.java
@@ -1,6 +1,9 @@
 package tools;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.math.BigInteger;
 
 import static org.junit.Assert.*;
 
@@ -8,5 +11,10 @@ public class CulcTest {
 
     @Test
     public void plus() {
+
+        Culc culc =  new Culc();
+      BigInteger actual =  culc.plus(BigInteger.valueOf(10), BigInteger.valueOf(2),BigInteger.valueOf(1));
+      BigInteger expected = BigInteger.valueOf(13);
+        Assert.assertEquals(expected, actual);
     }
 }
